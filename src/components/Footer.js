a7d3ea287809a5b31cafddc94c9645a64f5abd23
Max Morel <maxmorel@pisanvs.cl>
2024-10-29 20:20:33 -0300

    Add attribution to original authors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,6 @@
+import Link from "next/link"
+
+import RedText from "./utility/RedText";
 import Address from "@/components/Footers/Address";
 import Logos from "@/components/Footers/Logos";
 import UsefulLinks from "@/components/Footers/UsefulLinks";
@@ -18,7 +21,7 @@ const Footer = () => {
         <UsefulLinks />
       </section>
       <section className="footer_text flex flex-col-reverse items-center justify-between gap-3 text-center md:flex-row ">
-        <div>2024 TEDxLINTAC Youth</div>
+        <div>2024 TEDxLINTAC Youth (Code originally open-sourced by <RedText><Link href={"https://tedxgecrit.vercel.app/"} className="hover:underline">TEDxGECRIT</Link></RedText> team)</div>
         <div className="flex select-none flex-row justify-center gap-1">
           {/*<FooterIcon link={linkedinLink} iconName="linkedin" />*/}
           <FooterIcon link={instagramLink} iconName="instagram" />
