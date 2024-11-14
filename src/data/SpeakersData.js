68fe1f55da8eef61bff2de856ad6e2235ade4df2
Max Morel <maxmorel@pisanvs.cl>
2024-11-14 19:55:57 -0300

    Clean up.

--- src/data/SpeakersData.js
+++ src/data/SpeakersData.js
@@ -1,11 +1,3 @@
-import ar from "../../public/speakers/anaswara_ramesh.webp";
-import aa from "../../public/speakers/anoop_ambika.webp";
-import an from "../../public/speakers/adhila_noora.webp";
-import sk from "../../public/speakers/siddique_kappan.webp";
-import vv from "../../public/speakers/v_vigneshwari.webp";
-import np from "../../public/speakers/adv_navaneeth_p.webp";
-import pg from "../../public/speakers/priyesh_gopalakrishnan.webp";
-
 class Speaker {
   constructor(name, description, image) {
     this.name = name;
