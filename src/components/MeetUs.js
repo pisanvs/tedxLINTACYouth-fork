8d72e4cfb6837d31e124c5077433c3d41b28db7d
Max Morel <maxmorel@pisanvs.cl>
2024-11-17 20:29:37 -0300

    fix react error: non-existent tag

--- src/components/MeetUs.js
+++ src/components/MeetUs.js
@@ -9,9 +9,9 @@ export default function MeetUs() {
   return (
     <Container section="meetus">
       <div className="flex w-full flex-col items-center justify-center text-center">
-        <quote className="mb-[15px] w-[89%] text-[19px] font-light leading-tight md:w-[75%] md:text-[24px]">
+        <p className="mb-[15px] w-[89%] text-[19px] font-light leading-tight md:w-[75%] md:text-[24px]">
           {dictionary.teamThanks}
-        </quote>
+        </p>
         <button className="w-[185px] rounded-[3px] bg-red font-semibold">
           <Link
             href="/meet-us"
