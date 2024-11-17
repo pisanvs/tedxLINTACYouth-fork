f28f28c70fb4ec26557f6fa7d2f7df0bcf2b7f84
Max Morel <maxmorel@pisanvs.cl>
2024-11-17 20:30:59 -0300

    Make CTA button max-content

--- src/components/utility/GetTicketsButton.js
+++ src/components/utility/GetTicketsButton.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function NavActionButton() {
     const { dictionary } = useDict();
     return (
-        <button className="enabled w-[200px] select-none rounded-[3px] bg-red font-semibold text-white mr-8">
+        <button className="enabled w-max select-none rounded-[3px] bg-red font-semibold text-white mr-8">
             <Link
                 href={"https://forms.gle/HfVAVjydd9YNdT1z8"}
                 className="flex h-full w-full justify-center px-4 py-2 lg:py-1"
