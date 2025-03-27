39b5b1edb7fa479a19d5303b0023a362938981ad
Max Morel <maxmorel@pisanvs.cl>
2025-03-27 20:51:19 -0300

    change button bg

--- src/components/utility/GetTicketsButton.js
+++ src/components/utility/GetTicketsButton.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function NavActionButton() {
     const { dictionary } = useDict();
     return (
-        <button className="disabled w-max select-none rounded-[3px] bg-gray font-semibold text-white mr-8">
+        <button className="disabled w-max select-none rounded-[3px] bg-gray-800 font-semibold text-white mr-8">
             <Link
                 href={"about:blank"}
                 className="flex h-full w-full justify-center px-4 py-2 lg:py-1"
