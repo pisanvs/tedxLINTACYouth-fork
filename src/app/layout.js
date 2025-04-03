4c46ebb13dc24e9374d9d9bf2e8e68b6a2f0249a
pisanvs <maxmorel@pisanvs.cl>
2025-04-03 13:00:32 -0300

    (GTM) Add GTM tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from '@next/third-parties/google'
 import PoppinsFont from "@/utils/PoppinsFont";
 import "./globals.css";
 import { DictProvider } from "@/locales/dict";
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <PoppinsFont />
+      <GoogleTagManager gtmId="GTM-N25H7QRD" />
       <body className="selection:bg-red selection:text-white">
         <DictProvider>
           {children}
