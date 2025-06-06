e1d2055505ee255dd7a0d65a57178dc0065d4fc1
pisanvs <maxmorel@pisanvs.cl>
2025-06-06 13:25:32 -0400

    Update GetTicketsButton.js

--- src/components/utility/GetTicketsButton.js
+++ src/components/utility/GetTicketsButton.js
@@ -4,9 +4,9 @@ import Link from "next/link"
 export default function NavActionButton() {
     const { dictionary } = useDict();
     return (
-        <button className="disabled w-max select-none rounded-[3px] bg-gray-600 font-semibold text-white mr-8" disabled={true}>
+        <button className="w-max select-none rounded-[3px] bg-tedRed font-semibold text-white mr-8" disabled={true}>
             <Link
-                href={"#"}
+                href={"https://lu.ma/gpm7usxy"}
                 className="flex h-full w-full justify-center px-4 py-2 lg:py-1"
             >
                 { dictionary.navbar.cta }
